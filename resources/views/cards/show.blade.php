821dfcdede9a8edcce12b515e14e6db408a78e4b
Jan Drábek <JD-mac@Jan-MacBook-Pro-6.local>
2016-05-17 00:15:22 +0200

    Forms finished (#9)

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -1,12 +1,29 @@
 @extends('master')
 
 @section('content')
-  <h1>Card Detail:</h1>
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <h1>{{ $card->title }}</h1>
 
-  <p>{{ $card->title }}<p>
+      <ul clas="list-group">
+        @foreach ($card->notes as $note)
+          <li class="list-group-item">{{ $note->body }}</li>
+        @endforeach
+      </ul>
+      <hr>
 
-    @foreach ($card->notes as $note)
-      <li>{{ $note->body }}</li>
-    @endforeach
+      <h3>Add a New Note</h3>
+      <form method="POST" action="/cards/{{$card->id}}/notes">
+        <div class="form-group">
+          <textarea name="body" class="form-control"></textarea>
+        </div>
 
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Add Note</button>
+        </div>
+
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+      </form>
+    </div>
+  </div>
 @stop
