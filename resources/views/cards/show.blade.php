dc3f5c14c99fdaf5f67fc976f46a70bc9e053ea5
Jan Drábek <JD-mac@Jan-MacBook-Pro-6.local>
2016-05-17 01:43:38 +0200

    Update finished #10

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -7,7 +7,10 @@
 
       <ul clas="list-group">
         @foreach ($card->notes as $note)
-          <li class="list-group-item">{{ $note->body }}</li>
+          <li class="list-group-item">
+            <a href="/notes/{{ $note->id }}/edit">{{ $note->body }}</a>
+            <a href=# style="float:right">{{ $note->user->username }}</a>
+          </li>
         @endforeach
       </ul>
       <hr>
